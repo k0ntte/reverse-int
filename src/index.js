c69b793d0b80469c407421d7f5b02e5cfa39e455
k0ntte <bystrov_igor@bk.ru>
2023-05-12 17:12:16 +0300

    complite

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if(n<0){
+        n*=-1;
+    }
+    return Number(String(n).split("").reverse().join(""));
+};
+
+function a(n) {
+    if(n<0){
+        n*=-1;
+    }
+    return Number(String(n).split("").reverse().join(""));
+
 }
+
+console.log(a(-256));
